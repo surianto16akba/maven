73a72d6a4d704971c30d4be39ead2352209cb3cc
itsez1 <itsez1makassar@gmail.com>
2023-04-13 23:03:09 +0800

    first commit

---
+++ src/main/java/programer/zaman/now/maven/App.java
@@ -0,0 +1,13 @@
+package programer.zaman.now.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
